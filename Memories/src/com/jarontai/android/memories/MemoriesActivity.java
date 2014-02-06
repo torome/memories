ac29e198a2d5d3b9de833b93ae48f7b75fd84f08
JaronTai <jaroncn@gmail.com>
2014-02-06 13:13:46 +0800

    Init commit

---
+++ Memories/src/com/jarontai/android/memories/MemoriesActivity.java
@@ -0,0 +1,14 @@
+package com.jarontai.android.memories;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MemoriesActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_memories);
+	}
+
+}
