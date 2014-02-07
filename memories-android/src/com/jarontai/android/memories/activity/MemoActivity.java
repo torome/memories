08d23a1fefe2a283cc88eaf2a256d504209c55a7
JaronTai <jaroncn@gmail.com>
2014-02-07 16:50:18 +0800

    make MemoListActivity the main activity

--- memories-android/src/com/jarontai/android/memories/activity/MemoActivity.java
+++ memories-android/src/com/jarontai/android/memories/activity/MemoActivity.java
@@ -4,7 +4,7 @@ import com.jarontai.android.memories.fragment.MemoFragment;
 
 import android.support.v4.app.Fragment;
 
-public class MemoriesActivity extends SingleFragmentActivity {
+public class MemoActivity extends SingleFragmentActivity {
 
 	@Override
 	protected Fragment createFragment() {
