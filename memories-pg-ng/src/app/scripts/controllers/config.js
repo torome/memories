c0213ce238923f0e1fdb8ea16034021859f725fd
JaronTai <jaroncn@gmail.com>
2014-02-12 14:39:16 +0800

    add MainCtrl

---
+++ memories-pg-ng/src/app/scripts/controllers/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('memoriesApp')
+  .controller('ConfigCtrl', function ($scope) {
+    $scope.data = {};
+    $scope.fn = {};
+
+  });
--- memories-pg-ng/src/app/scripts/controllers/main.js
+++ memories-pg-ng/src/app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('srcApp')
+angular.module('memoriesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.data = {};
+    $scope.fn = {};
+
+    $scope.data.menuTip = "向右滑动或点击左上角按钮打开菜单."
+    $scope.data.emptyInfo = "照片列表为空，请赶快去拍照吧!";
   });
